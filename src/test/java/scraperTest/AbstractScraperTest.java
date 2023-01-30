d713c18175e6fa333d9b646e2f27b1be0600bb38
Nave-Cohen <117146523+Nave-Cohen@users.noreply.github.com>
2023-01-30 11:08:08 +0200

    Unit test (#7)
    * fix project build path for gradle
    
    * make gradlew excuteable
    
    * 2nd constructor for scrapers - Test, .html file to resource
    
    * add minimal scraper test
    
    * added description test
    
    * fixing abstractscraper test
    
    * fixing abstractscraper test

--- src/test/java/scraperTest/AbstractScraperTest.java
+++ src/test/java/scraperTest/AbstractScraperTest.java
@@ -26,15 +26,23 @@ public class AbstractScraperTest {
 
     @Test
     public void getPriceSuccess() {
-        Double expected = 1.81;
+        Double expected = 1.83;
         Double result = scraper.getPrice();
         assertEquals(expected, result);
     }
+
     @Test
-    public void getDiscountSuccess(){
+    public void getDiscountSuccess() {
         Integer expected = 89;
         Integer result = scraper.getDiscount();
         assertEquals(expected, result);
     }
 
+    @Test
+    public void getDescriptionSuccess() {
+        String expected = "Usb Heated Shoe Insoles Electric Foot Warming Pad Feet Warmer Sock Pad Mat Winter Outdoor Sports Heating Insole Winter Warm - Insoles - AliExpress";
+        String result = scraper.getDescription();
+        assertEquals(expected, result);
+    }
 }
+
